94582abbd706f269506cfc926315452397649fe7
vijaymarneni <vijay.marneni@gmail.com>
2022-11-27 08:05:45 +0530

    2021 Day2 puzzle
    See description here https://adventofcode.com/2021/day/2

---
+++ Day2.py
@@ -0,0 +1,36 @@
+def depthOfTravel():
+    f = open("D:\\User\\vmarneni\\Downloads\\puzzle2.csv","r", encoding="utf-8-sig")
+    l = 0
+    d = 0
+    for la in f:
+        li = la.split(" ")
+        x = int(li[1])
+        if li[0] == "forward":
+            l=l+x
+        if li[0] == "down":
+            d=d+x
+        if li[0] == "up":
+            d=d-x
+    return l,d, l*d
+
+
+def depthOfTravel():
+    f = open("D:\\User\\vmarneni\\Downloads\\puzzle2.csv","r", encoding="utf-8-sig")
+    l = 0
+    d = 0
+    depth = 0
+    for la in f:
+        li = la.split(" ")
+        x = int(li[1])
+        if li[0] == "forward":
+            if d != 0:
+                depth+=x*d
+            l=l+x
+        if li[0] == "down":
+            d=d+x
+        if li[0] == "up":
+            d=d-x
+    return l,d, depth,  l*depth
+
+
+
