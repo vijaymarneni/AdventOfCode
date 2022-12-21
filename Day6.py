1ef5a11a091288375719ffc9a8d00f0f571e8a0b
vijaymarneni <vijay.marneni@gmail.com>
2022-12-21 19:05:07 +0530

    Add files via upload
    Adding day 4, 5 and 6 code

---
+++ Day6.py
@@ -0,0 +1,5 @@
+def startOfPacket():
+    f = open("D:\\User\\vmarneni\\OneDrive - Altimetrik Corp\\Desktop\\2021\\Day5_Input_2022.txt","r", encoding="utf-8-sig")
+    for la in f:
+        print(la)
+    f.close()
